9ce2c20095f20d90348c5de2581e3873c7d7c54b
Rose <rosexu1996@yahoo.com>
2015-06-13 18:30:06 -0400

    selection works now

--- contentscript.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-	initVariables();
-	console.log("web page loaded!");
-});
-
--- javascripts/contentscript.js
+++ javascripts/contentscript.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
+	initVariables();
+	initEvents();
 	console.log("web page loaded!");
 });
+
+function initVariables() {
+	this.bod = $(document).find("body");
+}
+
+function initEvents() {
+	$(this.bod).bind('mouseup', function() {
+		console.log(window.getSelection().toString());
+	});
+}
