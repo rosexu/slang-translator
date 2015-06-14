dd98dbc8ba8bf4d0bfc8a2e5623ccc160dbd84a2
Rose <rosexu1996@yahoo.com>
2015-06-14 00:00:25 -0400

    added Yashen's code

--- javascripts/contentscript.js
+++ javascripts/contentscript.js
@@ -23,3 +23,24 @@ function initEvents() {
 		}
 	});
 }
+
+
+	
+function warnUser(word){	
+	if (checkifoneword(word))
+	{console.log("Please highlight only one word (multiword functionality may be added in future)");}
+	else {
+	    console.log(retrievedefinition());
+        console.log(retrievedefinitionurban());
+	}
+});
+
+
+
+function checkifoneword(a_string){
+
+    var parts = a_string.split(" ");
+    var length=parts.length;
+    if (length>1) {return true;}
+    else {return false;}
+}
