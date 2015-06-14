d368740951d922e7b681a863a3b022f21c8a2e6b
Shahzaib Gill <shahzaibgill@gmail.com>
2015-06-13 22:54:50 -0400

    renamed files

--- javascripts/apistuff.js
+++ javascripts/apistuff.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document ).ready(function() {
     console.log("web page loaded!");
     console.log(retrievedefinition());
     console.log(retrievedefinitionurban());
