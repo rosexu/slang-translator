8bc68adddbd80bf0ae2c5060ca5dcaa260a690fd
Rose <rosexu1996@yahoo.com>
2015-07-01 18:01:13 -0400

    formatting

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,88 +1,59 @@
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		console.log("request triggered: " + request);
-		console.log(sender.tab ?
-					"from a content script:" + sender.tab.url :
-					"from the extension");
-		if (request === "") {
-			sendResponse("");
-			return true;
-		}
-		else {
-			//sendUrbanAPICall(request, sendResponse);
-			implementdictionaryapi(request, sendResponse);
-			//implement_urban_api(request, sendResponse);
-			return true;
-		}
-	});
-
-
-function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
+function(request, sender, sendResponse) {
+// console.log("request triggered: " + request);
+// console.log(sender.tab ?
+//"from a content script:" + sender.tab.url :
+//"from the extension");
+    if (request === "") {
+        sendResponse("");
+        return true;
+    } else {
+        ImplementDictionaryAPI(request, sendResponse);
+        return true;
     }
-    return text;
-    console.log(getSelectionText());
-}
+});
  
-function implementdictionaryapi(word, callback){
-$.ajax({
-   url: "https://montanaflynn-dictionary.p.mashape.com/define?word=" + word,
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('X-Mashape-Key', 'A83cRYKgDUmshj54WtntYY9pUFnWp19CDK6jsnLqO8Rn3bBMh3');
-       xhr.setRequestHeader('Accept','application/json');
-   },
-   success: function(result) {
-   	   if (typeof result.definitions[0] === 'undefined') {
-   	   		console.log("word is undefined in normal dictionary");
-   	   		implement_urban_api(word, callback);
-   	   }
-   	   else {
-	   	   var aresult = JSON.stringify(result.definitions[0].text);
-	       console.log(aresult);
-	       callback(aresult); 
-	   }
-	}
-});  
-     
-}
- 
- 
-function implement_urban_api(word, callback){
-$.ajax({
-   url: "https://mashape-community-urban-dictionary.p.mashape.com/define?term=" + word,
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('X-Mashape-Key', 'A83cRYKgDUmshj54WtntYY9pUFnWp19CDK6jsnLqO8Rn3bBMh3');
-       xhr.setRequestHeader('Accept','text/plain');
-   },
-   success: function(result) { 
-   		if (typeof result.list[0] === 'undefined') {
-   	   		console.log("word is undefined in urbandictionary");
-   	   		callback("");
-   	   	}
-   	   	else {
-   	   		var aresult = JSON.stringify(result.list[0].definition);
-   	   		console.log(aresult);
-   	   		callback(aresult);
-   	   	}
-       }
-});  
-     
-}
- 
- 
-function retrievedefinition(word){
-    implementdictionaryapi(word, function(result1){
-        document.getElementById("output").innerHTML = result1;
- 
+function ImplementDictionaryAPI(word, callback){
+    $.ajax({
+        url: "https://montanaflynn-dictionary.p.mashape.com/define?word=" + word,
+        type: "GET",
+        beforeSend: function(xhr){
+            xhr.setRequestHeader('X-Mashape-Key', 'A83cRYKgDUmshj54WtntYY9pUFnWp19CDK6jsnLqO8Rn3bBMh3');
+            xhr.setRequestHeader('Accept','application/json');
+        },
+        success: function(result) {
+            if (typeof result.definitions[0] === 'undefined') {
+                console.log("word is undefined in normal dictionary");
+                ImplementUrbanAPI(word, callback);
+            }
+            else {
+                var aresult = JSON.stringify(result.definitions[0].text);
+                console.log(aresult);
+                callback(aresult);
+            }
+        }
     });
 }
  
-function retrievedefinitionurban(word){
-    implement_urban_api(word, function(result){
-        document.getElementById("output2").innerHTML = result;
+ 
+function ImplementUrbanAPI(word, callback){
+    $.ajax({
+        url: "https://mashape-community-urban-dictionary.p.mashape.com/define?term=" + word,
+        type: "GET",
+        beforeSend: function(xhr){
+            xhr.setRequestHeader('X-Mashape-Key', 'A83cRYKgDUmshj54WtntYY9pUFnWp19CDK6jsnLqO8Rn3bBMh3');
+            xhr.setRequestHeader('Accept','text/plain');
+        },
+        success: function(result) {
+            if (typeof result.list[0] === 'undefined') {
+                console.log("word is undefined in urbandictionary");
+                callback("");
+            }
+            else {
+                var aresult = JSON.stringify(result.list[0].definition);
+                console.log(aresult);
+                callback(aresult);
+            }
+        }
     });
 }
